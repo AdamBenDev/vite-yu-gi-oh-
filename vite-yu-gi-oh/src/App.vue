756a2c12bc136968db775a586398e885de2b5dbd
AdamBenDev <adamaya9732@gmail.com>
2023-04-18 06:24:21 +0200

    api linking and print on page

--- vite-yu-gi-oh/src/App.vue
+++ vite-yu-gi-oh/src/App.vue
@@ -1,37 +1,65 @@
 <template>
   <div>
-    <h1>yu-gi-oh</h1>
+    <h1>Yu-Gi-Oh Cards</h1>
+    <div v-if="isLoading" class="loader"></div>
+    <div class="card-container">
+      <Card v-for="(card, index) in cards" :key="index" :card="card"></Card>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-  export default {
-    data(){
-      return {
-        API_URL: "https://db.ygoprodeck.com/api/v7/cardinfo.php",
-        cards: null
-      }
+import axios from 'axios';
+import Card from './components/Card.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      apiEndpoint: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+      cards: [],
+      isLoading: true,
+    };
+  },
+  methods: {
+    getCards() {
+      axios.get(this.apiEndpoint).then((response) => {
+        this.cards = response.data.data.slice(0, 15);
+        this.isLoading = false;
+      });
     },
-    methods: {
-      callApi(url){
-        axios
-        .get(url)
-        .then(Response => {
-          console.log(Response);
-        })
-        .catch(err => {
-          console.log(err);
-          console.error(err.message);
-        })
-      }
-    },
-    mounted(){
-      this.callApi(this.API_URL);
-    }
-  }
+  },
+  mounted() {
+    this.getCards();
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style>
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
 
+.loader {
+  border: 16px solid #f3f3f3;
+  border-top: 16px solid #3498db;
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
---
+++ vite-yu-gi-oh/src/components/loader.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="loader">
+      <div class="loader-inner"></div>
+    </div>
+  </template>
+  
+  <style scoped>
+  .loader {
+    display: flex;
+  }
+   
