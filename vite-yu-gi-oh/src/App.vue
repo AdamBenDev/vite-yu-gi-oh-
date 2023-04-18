092124d90e7f65a6c5f8318a1b2af39e5a65a84c
AdamBenDev <adamaya9732@gmail.com>
2023-04-18 05:01:02 +0200

    right api link

--- vite-yu-gi-oh/src/App.vue
+++ vite-yu-gi-oh/src/App.vue
@@ -5,18 +5,27 @@
 </template>
 
 <script>
+import axios from 'axios'
   export default {
     data(){
       return {
-        API_URL: "https://ygoprodeck.com/api-guide/",
+        API_URL: "https://db.ygoprodeck.com/api/v7/cardinfo.php",
         cards: null
       }
     },
     methods: {
-      callApi(){
-        
+      callApi(url){
+        axios
+        .get(url)
+        .then(Response => {
+          console.log(Response);
+        })
+        .catch(err => {
+          console.log(err);
+          console.error(err.message);
+        })
       }
-    }
+    },
     mounted(){
       this.callApi(this.API_URL);
     }
